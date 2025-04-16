e61dec9f3b19ffbfc2a5d4ab890e7cda0003bbb3
RisanNasir <62825093+RisanNasir@users.noreply.github.com>
2025-04-16 06:23:50 +0100

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log(`my silly Name is ${sillyName}`);
+
+
+import superheroes from 'superheroes';
+import {randomSuperhero} from 'superheroes';
+console.log(`I am ${randomSuperhero()}`);
